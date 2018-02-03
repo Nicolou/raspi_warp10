18da2ee10aec1964fc85bebc243f2f1ce26ec3c4
Nicolou <nicolas.montarnal@free.fr>
2018-02-03 11:44:47 +0100

    first commit

---
+++ LISEZMOI.md
@@ -0,0 +1,24 @@
+# grafana et warp10 sur Raspberry PI
+
+ce projet présente la mise en oeuvre de Warp10.io et Grafana sur raspberry PI
+dans le but de récolter et d'exploiter des métriques provenant de capteurs externes.
+
+Le stockage et l'extration des métriques se fait à partir de l'application warp10 (http://warp10.io)
+Les données sont consultées via un navigateur web avec l'application Grafana  ( https://grafana.com/ )
+
+## vue des graphiques :
+![img](images/graf1.png)
+Format: ![Alt Text]( copy ecran grafana)
+
+## matériels en action :
+![matos](images/matos1.jpg)
+Format: ![Alt Text]( photo du rasp)
+
+
+
+
+
+
+
+
+
---
+++ src/sensorsProg/acquisitionv1/warp10Client.py
@@ -0,0 +1,54 @@
+﻿import logging
+import time
+import requests
+import sys
+
+
+class Warp10Client(object):
+	"""Object to POST data into warp10"""
+	def __init__(self, server, readToken, writeToken, appliId):
+		self._SERVER=server
+		self._TOKEN_READ=readToken
+		self._TOKEN_WRITE=writeToken
+		self._APPLI=appliId
+		self._stringToWrite=""
+		self._logger = logging.getLogger(__name__)
+
+
+	def addValueForWrite( self, name, labels, value ):
+		""" prepare value to push on warp10 """
+		# write timeStamp in warp10 format , ie microsecond since
+		self._stringToWrite += "{0:16.0f}".format(time.time()*1000000 ) + "// "
+		self._stringToWrite += self._APPLI + "." + name
+		if len(labels) > 0:
+			self._stringToWrite += "{"
+			for label, val in labels:
+				self._stringToWrite += label + "=" + val + ", "
+			#remove last ", "
+			self._stringToWrite.rstrip(", ")
+			self._stringToWrite += "} "
+		else:
+			self._stringToWrite += "{} "
+		#set value
+		self._stringToWrite += str(value) + '\n'
+		self._logger.debug(" warp10 data string = " + self._stringToWrite )
+
+
+	def pushValues( self ):
+		""" publish value to warp10 by sending http request """
+		request_url = self._SERVER + '/api/v0/update'
+		request_header = {'X-Warp10-Token' : self._TOKEN_WRITE }
+		request_data = self._stringToWrite
+		try:
+
+			self._logger.debug(" post data to " + request_url)
+			r = requests.request('POST', request_url, headers =request_header, data =request_data)
+			self._logger.debug(" response : " + r.text )
+
+		except requests.exceptions as ex:
+			self.error("une erreur dans la requete vers " + self._SERVER + " est survenue" , Ex )
+
+		#reinitialize the vlues string
+		self._stringToWrite = ""
+
+
